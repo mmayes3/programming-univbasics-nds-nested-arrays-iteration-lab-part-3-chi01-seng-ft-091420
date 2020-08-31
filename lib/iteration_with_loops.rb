870b49db036eddde09083a046e2072ba09729426
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-31 05:44:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
   count = 0
-  
+  string_array = []
+  while count < src.length do
+    count2 = 0 
+    while count2 < src[count].length do 
+      if src[count][count2].class == String 
+        string_array << src[count][count2]
+      end
+      count2 += 1
+    end
+    count += 1
+  end
+  p string_array.join(" ")
 end
